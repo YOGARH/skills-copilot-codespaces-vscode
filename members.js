d9f6e01770555a11cbdd3922dfc40aceee5710c1
Helina Freesgi <helina.yogaraj@gmail.com>
2024-10-20 17:51:06 +0000

    codespace 2 commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillsMembers() {
+  return [
+    {
+      name: 'John Doe',
+      skills: ['JavaScript', 'React', 'Node.js'],
+    },
+    {
+      name: 'Jane Doe',
+      skills: ['Vue', 'Python', 'Django'],
+    },
+    {
+      name: 'Jim Doe',
+      skills: ['Angular', 'Ruby', 'Ruby on Rails'],
+    },
+  ];
+}
